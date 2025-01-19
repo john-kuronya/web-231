e19aa2d17881073a95b1a373b85b9517ff13d56d
John <john.kuronya@outlook.com>
2025-01-19 15:44:20 -0500

    Third commit

--- week-2/project02-02/project02-02_txt.js
+++ week-2/project02-02/project02-02_txt.js
@@ -3,9 +3,23 @@
       Project 02-02
 
       Application to test for completed form
-      Author: 
-      Date:   
+      Author: John Kuronya
+      Date: 1/16/25
 
       Filename: project02-02.js
  */
- 
+
+// verifyForm function
+function verifyForm() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+
+// conditional operator to check if name, email, and phone are truthy
+  name && email && phone
+     ? window.alert("Thank you!")
+     : window.alert("Please fill in all fields");
+}
+
+// event listener for the id "submit"
+document.getElementById("submit").addEventListener("click", verifyForm);
