e85d29334c19dd1403e917ac891ec911317d8fee
John <john.kuronya@outlook.com>
2025-01-26 13:37:08 -0500

    Fourth Commit

--- week-3/project02-04/project02-04_txt.js
+++ week-3/project02-04/project02-04_txt.js
@@ -3,17 +3,50 @@
       Project 02-04
 
       Application to calculate the cost of a restaurant order plus tax
-      Author: 
-      Date:   
+      Author: John Kuronya
+      Date:  1/20/25
 
       Filename: project02-04.js
  */
- 
 
+// constants with initial values
+const CHICKEN_PRICE = 10.95, HALIBUT_PRICE = 13.95, BURGER_PRICE = 9.95, SALMON_PRICE = 18.95, SALAD_PRICE = 7.95, SALES_TAX = 0.07;
 
+// event listener when items are selected
+document.getElementById("chicken").addEventListener("click", calcTotal);
+document.getElementById("halibut").addEventListener("click", calcTotal);
+document.getElementById("burger").addEventListener("click", calcTotal);
+document.getElementById("salmon").addEventListener("click", calcTotal);
+document.getElementById("salad").addEventListener("click", calcTotal);
 
+// function to calculate the total cost
+function calcTotal() {
+  let cost = 0;
 
-// Function to display a numeric value as a text string in the format $##.## 
+  let buyChicken = document.getElementById("chicken").checked;
+  let buyHalibut = document.getElementById("halibut").checked;
+  let buyBurger = document.getElementById("burger").checked;
+  let buySalmon = document.getElementById("salmon").checked;
+  let buySalad = document.getElementById("salad").checked;
+
+  cost += buyChicken ? CHICKEN_PRICE : 0;
+  cost += buyHalibut ? HALIBUT_PRICE : 0;
+  cost += buyBurger ? BURGER_PRICE : 0;
+  cost += buySalmon ? SALMON_PRICE : 0;
+  cost += buySalad ? SALAD_PRICE : 0;
+
+  document.getElementById("foodTotal").innerHTML = formatCurrency(cost);
+
+  let tax = cost * SALES_TAX;
+
+  document.getElementById("foodTax").innerHTML = formatCurrency(tax);
+
+  let totalCost = cost + tax;
+
+  document.getElementById("totalBill").innerHTML = formatCurrency(totalCost);
+}
+
+// Function to display a numeric value as a text string in the format $##.##
  function formatCurrency(value) {
     return "$" + value.toFixed(2);
  }
